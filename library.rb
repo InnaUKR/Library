f1afb51a849163857f54a4ce3fbd28d15fde251b
innaUKR <innascherbak11@gmail.com>
2017-11-06 23:29:36 +0200

    find_greatest method instead of inverse

--- library.rb
+++ library.rb
@@ -18,7 +18,6 @@ class Library
 
   def add_author(name, biography)
     raise ArgumentError if @authors[name]
-    # doesn't copy the author's names
     @authors[name] = Author.new(name, biography)
   end
 
@@ -37,42 +36,38 @@ class Library
     @orders << Order.new(book_name, reader_name, date)
   end
 
-  def inverse(hash)
-    i = {}
-    hash.each_pair do |k, v|
-      if v.class == Array
-        v.each do |x|
-          i[x] = i.key?(x) ? [k, i[x]].flatten : k
-        end
-      else
-        i[v] = i.key?(v) ? [k, i[v]].flatten : k
-      end
-    end
-    i
-  end
-
+  # return hash {key~book_title: value ~ amount of this book in orders}
   def hesh_of_counted_book
     @orders.each_with_object(Hash.new(0)) do |order, counts|
       counts[order.book] += 1
     end
   end
 
+  def find_greatest(hash_something_amount)
+    sorted_array = hash_something_amount.sort_by(&:last).reverse
+    greatest = []
+    sorted_array.take_while { |x| sorted_array[0][1] == x[1] }.each do |x|
+      greatest << x[0]
+    end
+    greatest
+  end
+
   def find_most_popular_book
-    result = hesh_of_counted_book
-    inverse(result)[result.values.max]
+    hash_book_amount = hesh_of_counted_book
+    find_greatest(hash_book_amount)
   end
 
   def find_greatest_reader
-    result = @orders.each_with_object(Hash.new(0)) do |order, counts|
+    hash_reader_amount = @orders.each_with_object(Hash.new(0)) do |order, counts|
       counts[order.reader] += 1
     end
-    inverse(result)[result.values.max]
+    find_greatest(hash_reader_amount)
   end
 
   def count_readers_of_most_popular_book
-    result = hesh_of_counted_book
-    sorted_array_result = result.sort { |a1, a2| a2[1] <=> a1[1] }
-    sorted_array_result[0..2].inject(0) { |sum, x| sum + x[1] }
+    hash_book_number = hesh_of_counted_book
+    sorted_array = hash_book_number.sort_by(&:last).reverse
+    sorted_array[0..2].inject(0) { |sum, arr_book_amount| sum + arr_book_amount[1] }
   end
 
   def save(file)
@@ -97,14 +92,19 @@ class Library
     }
   end
 
-  def from_json(file)
-    data_hash = JSON.parse(file)
+  def authors_from_json(data_hash)
     data_hash['authors']&.each_value do |author|
       add_author(author['name'], author['biography'])
     end
+  end
+
+  def books_from_json(data_hash)
     data_hash['books']&.each_value do |book|
       add_book(book['title'], book['author'])
     end
+  end
+
+  def readers_from_json(data_hash)
     data_hash['readers']&.each_value do |reader|
       add_reader(reader['name'],
                  reader['email'],
@@ -112,8 +112,19 @@ class Library
                  reader['street'],
                  reader['house'])
     end
+  end
+
+  def orders_from_json(data_hash)
     data_hash['orders']&.each do |reader|
       add_order(reader['book'], reader['reader'], reader['date'])
     end
+  end
+
+  def from_json(file)
+    data_hash = JSON.parse(file)
+    authors_from_json(data_hash)
+    books_from_json(data_hash)
+    readers_from_json(data_hash)
+    orders_from_json(data_hash)
   end
 end
